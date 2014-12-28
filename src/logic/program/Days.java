ae5b910edfca6e30922c36f9e938ac9a5d7d207a
AnaGramm <anastasis.gramm2@gmail.com>
2014-12-28 18:50:44 +0200

    ;

--- src/logic/program/Days.java
+++ src/logic/program/Days.java
@@ -4,5 +4,5 @@ package logic.program;
  * Created by gramanas on 16-Dec-14.
  */
 public enum Days {
-    SUNDAY, MONDAY, TUESDAY, WETNESDAY, THURSDAY, FRIDAY, SATURDAY
+    SUNDAY, MONDAY, TUESDAY, WETNESDAY, THURSDAY, FRIDAY, SATURDAY;
 }
